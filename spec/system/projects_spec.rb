4a27c9beef303934a595aad1a892dce8299cf852
ikd-gifu <oki19ki@gmail.com>
2024-05-20 10:16:15 +0900

    8章完了

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -3,22 +3,21 @@ require 'rails_helper'
 RSpec.describe "Projects", type: :system do
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
-
+    sign_in user
     visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
 
     expect {
+      save_and_open_page
       click_link "New Project"
       fill_in "Name", with: "Test Project"
       fill_in "Description", with: "Trying out Capybara"
       click_button "Create Project"
-
-      expect(page).to have_content "Project was successfully created"
-      expect(page).to have_content "Test Project"
-      expect(page).to have_content "Owner: #{user.name}"
     }.to change(user.projects, :count).by(1)
+
+    aggregate_failures do
+      expect(page).to_not have_content "Project was successfully created"
+      expect(page).to have_content "Test Project"
+      expect(page).to_not have_content "Owner: #{user.name}"
+    end
   end
 end
